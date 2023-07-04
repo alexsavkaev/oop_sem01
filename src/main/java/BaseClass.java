5ed5036545334e78d2f743afba1d65f331064df5
alexsavkaev <123101083+alexsavkaev@users.noreply.github.com>
2023-07-04 21:58:28 +0300

    Initial commit. All classes added and basic interactions

---
+++ src/main/java/BaseClass.java
@@ -0,0 +1,25 @@
+public class Cat {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public Cat(int age) {
+        this.age = age;
+    }
+
+    public Cat(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
